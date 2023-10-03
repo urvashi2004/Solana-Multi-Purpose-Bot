aeb596ec91d899e6b99c368333ad6e8e07673b4f
Kartik Passricha <lowspecgamer2989@gmail.com>
2023-10-03 22:46:01 +0530

    major changes (all check)

--- multipurposebot.py
+++ multipurposebot.py
@@ -10,11 +10,28 @@ import yt_dlp
 import datetime
 import random
 import os
+from collections import defaultdict
+from random import shuffle
+import json
 
 #intends and prefix commands .....................................
 intents = discord.Intents.all()
 client=commands.Bot(command_prefix ='&',intents=intents)
 songs_folder = "Songs"
+playlists = defaultdict(list)
+current_song_index = defaultdict(int)
+playlist_file = "playlists.json"
+
+#playlist in json file....................................
+if os.path.isfile(playlist_file):
+    with open(playlist_file, "r") as file:
+        saved_playlists = json.load(file)
+        playlists.update(saved_playlists)
+
+#save playlist ...................................
+def save_playlists():
+    with open(playlist_file, "w") as file:
+        json.dump(playlists, file)
 
 #showing that the bot has started..................................
 @client.event
@@ -113,34 +130,87 @@ async def resume(ctx):
 
 #play command.............................................
 @client.command()
-async def play(ctx, *, song_choice: str = None):
-     voice_channel = ctx.author.voice.channel
+async def play(ctx, playlist_name: str = None):
+    global current_song_index
 
-     voice_client = ctx.voice_client
-     if voice_client:
+    voice_channel = ctx.author.voice.channel
+
+    # Check if the bot is already connected to a voice channel
+    voice_client = ctx.voice_client
+    if voice_client:
         await voice_client.disconnect()
-    
-     voice_client = await voice_channel.connect()
 
-     songs = [f for f in os.listdir(songs_folder) if f.endswith('.mp3')]
-     if not songs:
+    voice_client = await voice_channel.connect()
+
+    if playlist_name:
+        # Check if the user input is a playlist name
+        if playlist_name.lower() in playlists:
+            selected_playlist = playlists[playlist_name.lower()]
+            if not selected_playlist:
+                await ctx.send(f"The '{playlist_name}' playlist is empty. Use the &add command to add songs to it.")
+                return
+
+            # Play the entire playlist sequentially
+            source = os.path.join(songs_folder, selected_playlist[current_song_index[playlist_name.lower()]])
+            voice_client.play(discord.FFmpegPCMAudio(source))
+            await ctx.send(f"Now playing: {selected_playlist[current_song_index[playlist_name.lower()]]}")
+
+            # Increment the current song index for the selected playlist
+            current_song_index[playlist_name.lower()] = (current_song_index[playlist_name.lower()] + 1) % len(selected_playlist)
+        else:
+            await ctx.send(f"The '{playlist_name}' playlist does not exist. Use the &createplaylist command to create it.")
+    else:
+        await ctx.send("Please provide a playlist name as input.")
+
+@client.command()
+async def createplaylist(ctx, playlist_name: str):
+    if playlist_name.lower() not in playlists:
+        playlists[playlist_name.lower()] = []
+        current_song_index[playlist_name.lower()] = 0
+        await ctx.send(f"Playlist '{playlist_name}' created.")
+        save_playlists()
+    else:
+        await ctx.send(f"The playlist '{playlist_name}' already exists.")
+
+@client.command()
+async def add(ctx, playlist_name: str, *, song_choice: str):
+    if playlist_name.lower() not in playlists:
+        await ctx.send(f"The playlist '{playlist_name}' does not exist. Use the &createplaylist command to create it.")
+        return
+
+    songs = [f for f in os.listdir(songs_folder) if f.endswith('.mp3')]
+    if not songs:
         await ctx.send("No songs found in the 'songs' folder.")
         return
 
-     if song_choice:
-        song_choice = song_choice.lower()
-        matching_songs = [song for song in songs if song_choice in song.lower()]
-        if not matching_songs:
-            await ctx.send(f"No songs matching '{song_choice}' found.")
-            return
-        random_song = random.choice(matching_songs)
-     else:
-         random_song = random.choice(songs)
+    song_choice = song_choice.lower()
+    matching_songs = [song for song in songs if song_choice in song.lower()]
 
-     source = os.path.join(songs_folder, random_song)
-     voice_client.play(discord.FFmpegPCMAudio(source))
-     await ctx.send(f"Now playing: {random_song}")
-    
+    if not matching_songs:
+        await ctx.send(f"No songs matching '{song_choice}' found in the 'songs' folder.")
+        return
+
+    # Add the matching songs to the specified playlist
+    playlists[playlist_name.lower()].extend(matching_songs)
+    await ctx.send(f"{len(matching_songs)} song(s) added to the '{playlist_name}' playlist.")
+    save_playlists()
+
+@client.command()
+async def showplaylist(ctx, playlist_name: str):
+    if playlist_name.lower() not in playlists:
+        await ctx.send(f"The playlist '{playlist_name}' does not exist. Use the &createplaylist command to create it.")
+        return
+
+    selected_playlist = playlists[playlist_name.lower()]
+
+    if not selected_playlist:
+        await ctx.send(f"The '{playlist_name}' playlist is empty. Use the &add command to add songs to it.")
+        return
+
+    await ctx.send(f"Current Playlist '{playlist_name}':")
+    for i, song in enumerate(selected_playlist, start=1):
+        await ctx.send(f"{i}. {song}")
+
     
 #Truth game .................................................(Upgrade it on a daily basis)
 @client.command(pass_context=True)
@@ -156,10 +226,8 @@ async def truth(ctx):
     i="Who is your comfort character?"
     j="What's your weirdest fantasy?"
     l1=[a,b,c,d,e,f,g,h,i,j]
-    for i in range(0,10):
-        rand = str(round(time(),7))
-        rand2 = str(rand[::-1])
-    embed=discord.Embed(title="TRUTH" , description=l1[int(rand2[0])],color=0x44ff44)
+    shuffle(l1)
+    embed=discord.Embed(title="TRUTH" , description=l1[0],color=0x44ff44)
     await ctx.send(embed=embed)
 
 #Dare game.............................................................(Upgrade it on a daily basis)
@@ -176,10 +244,8 @@ async def dare(ctx):
     i="Kiss an item from your room (tongue involved)"
     j="Balance a steel utensil on your nose and walk around for 30 seconds."
     l2=[a,b,c,d,e,f,g,h,i,j]
-    for i in range(0,10):
-        rand = str(round(time(),7))
-        rand2 = str(rand[::-1])
-    embed=discord.Embed(title="DARE" , description=l2[int(rand2[0])],color=0x44ff44)
+    shuffle(l2)
+    embed=discord.Embed(title="DARE" , description=l2[0],color=0x44ff44)
     await ctx.send(embed=embed)
     
 #token key ..............................................................
